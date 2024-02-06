909aedc14f7801d840f617da8a16e0bb1a9eeb84
Nasar-creator <nasarbasha2003@gmail.com>
2024-02-06 10:35:13 +0530

    Changes done

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,13 +2,9 @@ const cloudinary=require('cloudinary').v2;
 const {CloudinaryStorage}=require("multer-storage-cloudinary")
 
 cloudinary.config({
-    // cloud_name:process.env.cloudinary_cloud_name,
-    // api_key:process.env.cloudinary_key,
-    // api_secret:process.env.cloudinary_secret
-    cloud_name:"db4wknrol",
-    api_key:486666712328217,
-    api_secret:"dylg4agNPZTR0Fr8vf_50iRsPoY"
-
+    cloud_name:process.env.cloudinary_cloud_name,
+    api_key:process.env.cloudinary_key,
+    api_secret:process.env.cloudinary_secret
 });
 const storage=new CloudinaryStorage({
     cloudinary,
